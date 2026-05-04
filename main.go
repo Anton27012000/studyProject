461af419688ed4ab45cc251471b51dc2b1402f30
Anton Lukyanchikov <antontotenkopf@gmail.com>
2026-05-04 12:31:12 +0300

    День 1. Установка и первая программа

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 func main() {
 	name := "gopher"
 	number := 27
+	fmt.Println("My first project")
 	fmt.Printf("Hello and welcome, %s%d!\n", name, number)
 
 	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
+		fmt.Printf("Hello and welcome, %s%d!\n", name, i)
 	}
+
 }
