68070a310734b11693be481773f9e41e0d3b90ce
Anton Lukyanchikov <antontotenkopf@gmail.com>
2026-05-19 17:41:10 +0300

    День 6. Мини-проект: Консольный менеджер задач

--- task-manager/storage/storage.go
+++ task-manager/storage/storage.go
@@ -1,1 +1,161 @@
 package storage
+
+import (
+	"awesomeProject/task-manager/models"
+	"awesomeProject/task-manager/utils"
+	"strings"
+)
+
+type TaskStorage struct {
+	Tasks  []models.Task
+	NextID int
+}
+
+func NewTaskStorage() *TaskStorage {
+
+	return &TaskStorage{
+		Tasks:  []models.Task{},
+		NextID: 1,
+	}
+}
+
+func (s *TaskStorage) AddTask(
+	title string,
+	priority string,
+) {
+
+	title = utils.NormalizeTitle(title)
+
+	if title == "" {
+		return
+	}
+
+	task := models.Task{
+		ID:       s.NextID,
+		Title:    title,
+		Priority: priority,
+		Done:     false,
+	}
+
+	s.Tasks = append(s.Tasks, task)
+
+	s.NextID++
+}
+
+func (s *TaskStorage) FindByID(
+	id int,
+) (*models.Task, bool) {
+
+	for i := range s.Tasks {
+
+		if s.Tasks[i].ID == id {
+			return &s.Tasks[i], true
+		}
+	}
+
+	return nil, false
+}
+
+func (s *TaskStorage) DeleteByID(id int) bool {
+
+	for i := range s.Tasks {
+
+		if s.Tasks[i].ID == id {
+
+			s.Tasks = append(
+				s.Tasks[:i],
+				s.Tasks[i+1:]...,
+			)
+
+			return true
+		}
+	}
+
+	return false
+}
+
+func (s *TaskStorage) GetActiveTasks() []models.Task {
+
+	result := []models.Task{}
+
+	for _, task := range s.Tasks {
+
+		if !task.Done {
+			result = append(result, task)
+		}
+	}
+
+	return result
+}
+
+func (s *TaskStorage) SearchByTitle(
+	query string,
+) []models.Task {
+
+	query = strings.ToLower(
+		strings.TrimSpace(query),
+	)
+
+	result := []models.Task{}
+
+	for _, task := range s.Tasks {
+
+		title := strings.ToLower(task.Title)
+
+		if strings.Contains(title, query) {
+			result = append(result, task)
+		}
+	}
+
+	return result
+}
+
+func (s *TaskStorage) CompleteTask(
+	id int,
+) bool {
+
+	task, ok := s.FindByID(id)
+
+	if !ok {
+		return false
+	}
+
+	task.Done = true
+
+	return true
+}
+
+func (s *TaskStorage) GetStats() map[string]int {
+
+	stats := map[string]int{
+		"all":    len(s.Tasks),
+		"done":   0,
+		"active": 0,
+		"high":   0,
+		"medium": 0,
+		"low":    0,
+	}
+
+	for _, task := range s.Tasks {
+
+		if task.Done {
+			stats["done"]++
+		} else {
+			stats["active"]++
+		}
+
+		switch task.Priority {
+
+		case "high":
+			stats["high"]++
+
+		case "medium":
+			stats["medium"]++
+
+		case "low":
+			stats["low"]++
+		}
+	}
+
+	return stats
+}
--- task-manager/tests/task_test.go
+++ task-manager/tests/task_test.go
@@ -1,1 +1,126 @@
 package tests
+
+import (
+	"awesomeProject/task-manager/storage"
+	"awesomeProject/task-manager/utils"
+	"testing"
+)
+
+func TestAddTask(t *testing.T) {
+
+	taskStorage := storage.NewTaskStorage()
+
+	taskStorage.AddTask(
+		"Learn Go",
+		"high",
+	)
+
+	if len(taskStorage.Tasks) != 1 {
+		t.Error("task was not added")
+	}
+}
+
+func TestAddTaskEmptyTitle(t *testing.T) {
+
+	taskStorage := storage.NewTaskStorage()
+
+	taskStorage.AddTask(
+		"   ",
+		"high",
+	)
+
+	if len(taskStorage.Tasks) != 0 {
+		t.Error(
+			"empty task should not be added",
+		)
+	}
+}
+
+func TestFindByID(t *testing.T) {
+
+	taskStorage := storage.NewTaskStorage()
+
+	taskStorage.AddTask(
+		"Learn Go",
+		"high",
+	)
+
+	task, ok := taskStorage.FindByID(1)
+
+	if !ok {
+		t.Fatal("task should exist")
+	}
+
+	if task.Title != "Learn go" {
+		t.Error("wrong task returned")
+	}
+}
+
+func TestCompleteTask(t *testing.T) {
+
+	taskStorage := storage.NewTaskStorage()
+
+	taskStorage.AddTask(
+		"Learn Go",
+		"high",
+	)
+
+	taskStorage.CompleteTask(1)
+
+	task, _ := taskStorage.FindByID(1)
+
+	if !task.Done {
+		t.Error(
+			"task should be completed",
+		)
+	}
+}
+
+func TestStats(t *testing.T) {
+
+	taskStorage := storage.NewTaskStorage()
+
+	taskStorage.AddTask(
+		"Task 1",
+		"high",
+	)
+
+	taskStorage.AddTask(
+		"Task 2",
+		"low",
+	)
+
+	taskStorage.CompleteTask(1)
+
+	stats := taskStorage.GetStats()
+
+	if stats["done"] != 1 {
+		t.Error(
+			"done count should be 1",
+		)
+	}
+
+	if stats["active"] != 1 {
+		t.Error(
+			"active count should be 1",
+		)
+	}
+}
+
+func TestNormalizeTitle(t *testing.T) {
+
+	result := utils.NormalizeTitle(
+		"   пРИВЕТ МИР   ",
+	)
+
+	expected := "Привет мир"
+
+	if result != expected {
+
+		t.Errorf(
+			"expected %s, got %s",
+			expected,
+			result,
+		)
+	}
+}
