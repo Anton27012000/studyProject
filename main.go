cec811e7ecc88f97675817f49c3e741b9ccca683
Anton Lukyanchikov <antontotenkopf@gmail.com>
2026-05-19 14:45:27 +0300

    ень 4. Struct: свои типы и понятные сущности

--- main.go
+++ main.go
@@ -1,46 +1,20 @@
 package main
 
-import "awesomeProject/week1"
+import (
+	"awesomeProject/week2"
+	"fmt"
+)
 
 func main() {
 
-	week1.FirstProject()
+	students := []week2.Student{
+		{0, "Anton", 5.0},
+		{1, "Bob", 4.2},
+		{2, "Tom", 3.7},
+	}
+	student := week2.FindStudentByID(students, 0)
 
-	week1.PrintHelloLoop()
-
-	week1.Calculator()
-
-	week1.ConvertTemperature()
-
-	week1.MinutesToHours()
-
-	week1.CircleArea()
-
-	week1.CheckAge()
-
-	week1.ParseString()
-
-	week1.ConvertNumbersToString()
-
-	week1.MaxOfTwo()
-
-	week1.MaxOfThree()
-
-	week1.DiscountCalculator()
-
-	week1.ScoreGrade()
-
-	week1.SeasonByMonth()
-
-	week1.LoginValidation()
-
-	week1.SumToN()
-
-	week1.Factorial()
-
-	week1.MultiplicationTable()
-
-	week1.EvenOddCounter()
-
-	week1.GuessNumber()
+	if student != nil {
+		fmt.Println(student)
+	}
 }
